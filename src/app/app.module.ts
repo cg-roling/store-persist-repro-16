2652b4c79551559bc340f31c09e7a9ba5066c9c7
Nate Roling <roling@cartegraph.com>
2023-05-11 12:59:06 -0500

    Demo issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PersistStateModule } from '@ngrx-addons/persist-state';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 
+const reducers = {
+  demo: (x: any) => x,
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers),
+    PersistStateModule.forRoot<typeof reducers>({
+      states: [],
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
